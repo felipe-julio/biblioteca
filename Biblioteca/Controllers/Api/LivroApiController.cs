f013fa4a97660a3620c6c2333552d58f80c7da4b
felipe <phelipejulio16@gmail.com>
2018-09-10 00:55:16 -0300

    Adiciona tabela no index e exclui métodos desnecessarios nos controllers

---
+++ Biblioteca/Controllers/Api/LivroApiController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Biblioteca.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroApiController : Controller
+    {
+        private readonly BibliotecaContexto bibliotecaContexto;
+
+        public LivroApiController(BibliotecaContexto bibliotecaContexto)
+        {
+            this.bibliotecaContexto = bibliotecaContexto;
+        }
+
+        [HttpGet]
+        [Route("livrosativos")]
+        public IActionResult GetAll()
+        {
+            var clientes = bibliotecaContexto.Livros.ToList().Where(x => x.Situacao == 'A');
+
+            return Ok(clientes);
+        }
+    }
+}
